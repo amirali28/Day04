f41813c010621bf05170d3244f0dc6f54cb722a3
Amir <muharemovic.amir@gmail.com>
2021-03-12 23:04:10 +0100

    Izmjena filea

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,9 +1,9 @@
 function main() {
     var myDecimal = 8.2;
-    var myDecimalIE = 254e-5;
+    var myDecimalE = 254e-5;
     return {
         myDecimal,
-        myDecimalIE
+        myDecimalE
     };
 
 }
